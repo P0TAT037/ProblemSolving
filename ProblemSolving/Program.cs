c2c68f94e83290c88b27fe932846b0fa2774f050
Eslam <EEA@corelia.ai>
2024-02-20 12:11:37 +0200

    added ValidPalindrom

--- ProblemSolving/Program.cs
+++ ProblemSolving/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using ProblemSolving.TwoPointers;
+
+ValidPalindrom.Solution2("A man, a plan, a canal: Panama");
+ValidPalindrom.Solution2("race a car");
+ValidPalindrom.Solution2("aa");
+ValidPalindrom.Solution2("0P");
---
+++ ProblemSolving/TwoPointers/ValidPalindrom.cs
@@ -0,0 +1,65 @@
+﻿namespace ProblemSolving.TwoPointers;
+
+// https://leetcode.com/problems/valid-palindrome/?envType=study-plan-v2&envId=top-interview-150
+public class ValidPalindrom
+{
+    public static bool Solution(string s)
+    {
+        s = s.ToLower();
+        string ss = "";
+        foreach (char c in s)
+        {
+            if (char.IsLetterOrDigit(c))
+            {
+                ss += c;
+            }
+        }
+        
+        int i = 0;
+        int j = (ss.Length == 0) ? 0 : ss.Length-1;
+
+        while (i < j)
+        {
+            if (ss[i] != ss[j])
+                return false;
+        
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+    public static bool Solution2(string s)
+    {
+        s = s.ToLower();
+        
+        int i = 0;
+        int j = (s.Length == 0) ? 0 : s.Length-1;
+
+        while (i < j)
+        {
+            
+            if (!char.IsLetterOrDigit(s[i]))
+            {
+                i++;
+                continue;
+            }
+            
+            if (!char.IsLetterOrDigit(s[j]))
+            {
+                j--;
+                continue;
+            }
+            
+            if (s[i] != s[j])
+                return false;
+        
+            i++;
+            j--;
+        }
+        
+        return true;
+    }
+    
+}
